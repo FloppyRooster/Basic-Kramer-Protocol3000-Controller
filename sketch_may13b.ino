4903ac67147b84d7400fb40027c6307a35ace47c
Floppy_Rooster <jem.widdup@gmail.com>
2025-06-01 14:11:55 +1000

    fixed the keypad so all the buttons work

--- sketch_may13b.ino
+++ sketch_may13b.ino
@@ -6,14 +6,21 @@ const byte ROWS = 4;
 const byte COLS = 4; 
 
 //This assigns each key to a unique identifier, done this way so the buttons can be easily rotated, swapped ect.
-int hexaKeys[ROWS][COLS] = {
+/*int hexaKeys[ROWS][COLS] = {
   {1, 2, 3, 4},
   {5, 6, 7, 8},
   {9, 10, 11, 12},
   {13, 14, 15, 16}
+};*/
+
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2','3'},
+  {'4','5','6','7'},
+  {'8','9','A','B'},
+  {'C','D','E','F'}
 };
 
-byte rowPins[ROWS] = {5, 4, 3, 2}; 
+byte rowPins[ROWS] = {5, 4, 3, 2};
 byte colPins[COLS] = {6, 7, 8, 9}; 
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
@@ -32,8 +39,8 @@ char commands[16][64] = {
   "#VID 4>1,4>2,4>3,4>4\r", // All into 4
 
   //2nd Row More Customs
-  "#VID 1>1,3>2,1>3,1>4\r", //Blank
-  "#VID 3>1,1>2,3>3,3>4\r", //Blank
+  "#VID 1>1,3>2,1>3,1>4\r", 
+  "#VID 3>1,1>2,3>3,3>4\r", 
   "#VID 0>1,0>2,0>3,0>4\r", //Blank
   "#VID 0>1,0>2,0>3,0>4\r", //Blank
 
@@ -53,19 +60,27 @@ char commands[16][64] = {
 void setup(){
   // start communication with baud rate 9600
   mySerial.begin(9600);  
-
+  Serial.begin(9600);//USB deguging output
   // wait a moment to allow serial ports to initialize
   delay(100);
-
+  
 }
 
 //Standard Protocol alignment v
 //mySerial.write("#VID 1>1,2>2,3>3,4>4\r");
 void loop(){
   char customKey = customKeypad.getKey();
-
+  int commandIndex = customKey - '0'; //Becuase fuck you this converts char to int
+  if (commandIndex > 9) {
+    commandIndex -= 7; // Because there are 7 charachters between 9 and A in ascii 
+  }
+ 
+  
   //Literally just runs the command number as defined in the commands variable.
   if (customKey){
-    mySerial.write(commands[customKey]);
+    //mySerial.write(commands[customKey]);
+    Serial.println(commands[commandIndex]);
+    //Serial.println(customKey);
+
   }
 }
